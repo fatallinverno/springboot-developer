a4bc1417d0bf8b8c26638c2f4523740f64313ffe
linverno <fatal_linverno@naver.com>
2025-07-29 13:51:07 +0900

    UserRepository 추가

---
+++ src/main/java/io/springboot/linverno/developer/infrastructure/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package io.springboot.linverno.developer.infrastructure.repository;
+
+import io.springboot.linverno.developer.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
