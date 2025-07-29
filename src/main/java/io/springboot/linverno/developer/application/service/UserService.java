090b150f8ae4004cd809d779480cd5da1756766b
linverno <fatal_linverno@naver.com>
2025-07-29 13:50:44 +0900

    유저 회원가입 Service 기능 추가

---
+++ src/main/java/io/springboot/linverno/developer/application/service/UserService.java
@@ -0,0 +1,24 @@
+package io.springboot.linverno.developer.application.service;
+
+import io.springboot.linverno.developer.domain.entity.User;
+import io.springboot.linverno.developer.dto.articledto.AddUserRequest;
+import io.springboot.linverno.developer.infrastructure.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+
+}
