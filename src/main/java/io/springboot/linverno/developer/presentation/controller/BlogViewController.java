ce3d7df1e89765b34e432ab9eac47b7b4ae5d7e9
linverno <fatal_linverno@naver.com>
2025-07-29 11:07:03 +0900

    게시글 수정 컨트롤러 추가

--- src/main/java/io/springboot/linverno/developer/presentation/controller/BlogViewController.java
+++ src/main/java/io/springboot/linverno/developer/presentation/controller/BlogViewController.java
@@ -3,11 +3,13 @@ package io.springboot.linverno.developer.presentation.controller;
 import io.springboot.linverno.developer.application.service.BlogService;
 import io.springboot.linverno.developer.domain.entity.Article;
 import io.springboot.linverno.developer.dto.response.ArticleListViewResponse;
+import io.springboot.linverno.developer.dto.response.ArticleResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -31,9 +33,22 @@ public class BlogViewController {
     @GetMapping("/articles/{id}")
     public String getArticle(@PathVariable Long id, Model model) {
         Article article = blogService.findById(id);
-        model.addAttribute("article", new ArticleListViewResponse(article));
+        model.addAttribute("article", new ArticleResponse(article));
 
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+
+        if (id == null) {
+            model.addAttribute("article", new ArticleResponse());
+        } else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleResponse(article));
+        }
+
+        return "newArticle";
     }
 
 }
