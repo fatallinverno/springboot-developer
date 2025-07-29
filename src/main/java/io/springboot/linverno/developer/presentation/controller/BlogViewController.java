56f4397748654abfbd91e89fafb1ad75d6638ea2
linverno <fatal_linverno@naver.com>
2025-07-29 13:49:46 +0900

    Response 변경

--- src/main/java/io/springboot/linverno/developer/presentation/controller/BlogViewController.java
+++ src/main/java/io/springboot/linverno/developer/presentation/controller/BlogViewController.java
@@ -3,7 +3,7 @@ package io.springboot.linverno.developer.presentation.controller;
 import io.springboot.linverno.developer.application.service.BlogService;
 import io.springboot.linverno.developer.domain.entity.Article;
 import io.springboot.linverno.developer.dto.response.ArticleListViewResponse;
-import io.springboot.linverno.developer.dto.response.ArticleResponse;
+import io.springboot.linverno.developer.dto.response.ArticleViewResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,31 +21,29 @@ public class BlogViewController {
 
     @GetMapping("/articles")
     public String getArticles(Model model) {
-
-        List<ArticleListViewResponse> articles = blogService.findAll().stream().map(ArticleListViewResponse::new).toList();
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
 
         model.addAttribute("articles", articles);
 
         return "articleList";
-
     }
 
     @GetMapping("/articles/{id}")
     public String getArticle(@PathVariable Long id, Model model) {
         Article article = blogService.findById(id);
-        model.addAttribute("article", new ArticleResponse(article));
-
+        model.addAttribute("article", new ArticleViewResponse(article));
         return "article";
     }
 
     @GetMapping("/new-article")
     public String newArticle(@RequestParam(required = false) Long id, Model model) {
-
         if (id == null) {
-            model.addAttribute("article", new ArticleResponse());
+            model.addAttribute("article", new ArticleViewResponse());
         } else {
             Article article = blogService.findById(id);
-            model.addAttribute("article", new ArticleResponse(article));
+            model.addAttribute("article", new ArticleViewResponse(article));
         }
 
         return "newArticle";
