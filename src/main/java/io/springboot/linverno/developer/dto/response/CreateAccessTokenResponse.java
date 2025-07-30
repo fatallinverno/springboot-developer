793a85f240da8a31b4745a60deccc4b6c06824f1
linverno <fatal_linverno@naver.com>
2025-07-30 11:43:11 +0900

    토큰 생성 Response생성

---
+++ src/main/java/io/springboot/linverno/developer/dto/response/CreateAccessTokenResponse.java
@@ -0,0 +1,12 @@
+package io.springboot.linverno.developer.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CreateAccessTokenResponse {
+
+    private String accessToken;
+
+}
