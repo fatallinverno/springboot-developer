ab5cc1e90d92d09ccc3bbf016b90c91d6c4cafec
linverno <fatal_linverno@naver.com>
2025-07-30 11:46:34 +0900

    Jwt 컴포넌트 추가

---
+++ src/main/java/io/springboot/linverno/developer/config/jwt/JwtProperties.java
@@ -0,0 +1,15 @@
+package io.springboot.linverno.developer.config.jwt;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties("jwt")
+public class JwtProperties {
+
+    private String issuer;
+    private String secretKey;
+
+}
